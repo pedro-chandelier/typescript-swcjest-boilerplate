c6d3f423ed0ba2bb515acadb53aa58741963c51e
pedro_haeser <haeser.pedro@gmail.com>
2022-03-20 22:09:34 -0300

    remove boilerplate tests

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { multiply } from "."
-
-describe("multiply", () => {
-  it("is return correct number?", () => {
-    expect(multiply(10, 6)).toBe(60)
-  })
-})
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-function multiply(first: number, second: number): number {
-  return first * second
-}
-export { multiply }
